c865ec9ab9d5caa0cf31c6e58096b6559cfb3fc2
Adithya J <adithya.j@adithya321.me>
2016-11-01 20:10:13 +0530

    Update Copyright

--- ate/src/main/java/com/afollestad/appthemeengine/tagprocessors/TagProcessor.java
+++ ate/src/main/java/com/afollestad/appthemeengine/tagprocessors/TagProcessor.java
@@ -1,3 +1,21 @@
+/*
+ * Calculator Begins
+ * Copyright (C) 2016  Adithya J
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 package com.afollestad.appthemeengine.tagprocessors;
 
 import android.content.Context;
@@ -29,6 +47,7 @@ public abstract class TagProcessor {
     private static final String PRIMARY_COLOR_DEPENDENT = "primary_color_dependent";
     private static final String ACCENT_COLOR_DEPENDENT = "accent_color_dependent";
     private static final String WINDOW_BG_DEPENDENT = "window_bg_dependent";
+
     public TagProcessor() {
     }
 
--- ate/src/main/java/com/afollestad/appthemeengine/util/EdgeGlowUtil.java
+++ ate/src/main/java/com/afollestad/appthemeengine/util/EdgeGlowUtil.java
@@ -1,3 +1,21 @@
+/*
+ * Calculator Begins
+ * Copyright (C) 2016  Adithya J
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 package com.afollestad.appthemeengine.util;
 
 import android.annotation.TargetApi;
@@ -40,6 +58,7 @@ public final class EdgeGlowUtil {
     private static Field RECYCLER_VIEW_FIELD_EDGE_GLOW_BOTTOM;
     private static Field VIEW_PAGER_FIELD_EDGE_GLOW_LEFT;
     private static Field VIEW_PAGER_FIELD_EDGE_GLOW_RIGHT;
+
     private EdgeGlowUtil() {
     }
 
