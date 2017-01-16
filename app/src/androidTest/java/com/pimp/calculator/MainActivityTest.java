dc9266958e2b2cfc83a854648e3f937ffa442aec
Adithya J <adithya.j@adithya321.me>
2017-01-16 08:44:43 +0530

    :sparkles: :white_check_mark: Get started with Espresso

---
+++ app/src/androidTest/java/com/pimp/calculator/MainActivityTest.java
@@ -0,0 +1,49 @@
+/*
+ * Calculator Begins
+ * Copyright (C) 2017  Adithya J
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.pimp.calculator;
+
+import android.os.SystemClock;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.TimeUnit;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void shouldBeAbleToLaunchMainScreen() {
+        SystemClock.sleep(TimeUnit.SECONDS.toMillis(2));
+
+        onView(withText("DEG")).check(ViewAssertions.matches(isDisplayed()));
+    }
+}
